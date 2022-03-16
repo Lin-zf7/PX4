junwoo091400/PX4-Autopilot

PreFlightCheck: remove unused device_id argument

src/modules/commander/Arming/PreFlightCheck/checks/accelerometerCheck.cpp
@@ -62,7 +62,7 @@ bool PreFlightCheck::isAccelRequired(const uint8_t instance)
 }
 
 bool PreFlightCheck::accelerometerCheck(orb_advert_t *mavlink_log_pub, vehicle_status_s &status, const uint8_t instance,
-					const bool optional, int32_t &device_id, const bool report_fail)
+					const bool optional, const bool report_fail)
 {
 	const bool exists = (orb_exists(ORB_ID(sensor_accel), instance) == PX4_OK);
 	bool calibration_valid = false;
@@ -80,13 +80,11 @@ bool PreFlightCheck::accelerometerCheck(orb_advert_t *mavlink_log_pub, vehicle_s
 			}
 		}
 
-		device_id = accel.get().device_id;
-
 		if (status.hil_state == vehicle_status_s::HIL_STATE_ON) {
 			calibration_valid = true;
 
 		} else {
-			calibration_valid = (calibration::FindCalibrationIndex("ACC", device_id) >= 0);
+			calibration_valid = (calibration::FindCalibrationIndex("ACC", accel.get().device_id) >= 0);
 		}
 
 		if (!calibration_valid) {

src/modules/commander/Arming/PreFlightCheck/checks/gyroCheck.cpp
@@ -61,7 +61,7 @@ bool PreFlightCheck::isGyroRequired(const uint8_t instance)
 }
 
 bool PreFlightCheck::gyroCheck(orb_advert_t *mavlink_log_pub, vehicle_status_s &status, const uint8_t instance,
-			       const bool optional, int32_t &device_id, const bool report_fail)
+			       const bool optional, const bool report_fail)
 {
 	const bool exists = (orb_exists(ORB_ID(sensor_gyro), instance) == PX4_OK);
 	bool calibration_valid = false;
@@ -79,13 +79,11 @@ bool PreFlightCheck::gyroCheck(orb_advert_t *mavlink_log_pub, vehicle_status_s &
 			}
 		}
 
-		device_id = gyro.get().device_id;
-
 		if (status.hil_state == vehicle_status_s::HIL_STATE_ON) {
 			calibration_valid = true;
 
 		} else {
-			calibration_valid = (calibration::FindCalibrationIndex("GYRO", device_id) >= 0);
+			calibration_valid = (calibration::FindCalibrationIndex("GYRO", gyro.get().device_id) >= 0);
 		}
 
 		if (!calibration_valid) {

src/modules/commander/Arming/PreFlightCheck/checks/magnetometerCheck.cpp
@@ -61,7 +61,7 @@ bool PreFlightCheck::isMagRequired(const uint8_t instance)
 }
 
 bool PreFlightCheck::magnetometerCheck(orb_advert_t *mavlink_log_pub, vehicle_status_s &status, const uint8_t instance,
-				       const bool optional, int32_t &device_id, const bool report_fail)
+				       const bool optional, const bool report_fail)
 {
 	const bool exists = (orb_exists(ORB_ID(sensor_mag), instance) == PX4_OK);
 	bool calibration_valid = false;
@@ -80,13 +80,11 @@ bool PreFlightCheck::magnetometerCheck(orb_advert_t *mavlink_log_pub, vehicle_st
 			}
 		}
 
-		device_id = magnetometer.get().device_id;
-
 		if (status.hil_state == vehicle_status_s::HIL_STATE_ON) {
 			calibration_valid = true;
 
 		} else {
-			calibration_valid = (calibration::FindCalibrationIndex("MAG", device_id) >= 0);
+			calibration_valid = (calibration::FindCalibrationIndex("MAG", magnetometer.get().device_id) >= 0);
 		}
 
 		if (!calibration_valid) {
@@ -98,7 +96,7 @@ bool PreFlightCheck::magnetometerCheck(orb_advert_t *mavlink_log_pub, vehicle_st
 		for (uint8_t i = 0; i < ORB_MULTI_MAX_INSTANCES; i++) {
 			uORB::SubscriptionData<estimator_status_s> estimator_status_sub{ORB_ID(estimator_status), i};
 
-			if (estimator_status_sub.get().mag_device_id == static_cast<uint32_t>(device_id)) {
+			if (estimator_status_sub.get().mag_device_id == static_cast<uint32_t>(magnetometer.get().device_id)) {
 				if (estimator_status_sub.get().control_mode_flags & (1 << estimator_status_s::CS_MAG_FAULT)) {
 					is_mag_fault = true;
 					break;

Lin-zf7/PX4

Former-commit-id: b1c1163ee497be59462b7ec22bbdf05ac6be29ca

src/modules/commander/Arming/PreFlightCheck/checks/accelerometerCheck.cpp
@@ -62,7 +62,7 @@ bool PreFlightCheck::isAccelRequired(const uint8_t instance)
 }
 
 bool PreFlightCheck::accelerometerCheck(orb_advert_t *mavlink_log_pub, vehicle_status_s &status, const uint8_t instance,
-					const bool optional, int32_t &device_id, const bool report_fail)
+					const bool optional, const bool report_fail)
 {
 	const bool exists = (orb_exists(ORB_ID(sensor_accel), instance) == PX4_OK);
 	bool calibration_valid = false;
@@ -80,13 +80,11 @@ bool PreFlightCheck::accelerometerCheck(orb_advert_t *mavlink_log_pub, vehicle_s
 			}
 		}
 
-		device_id = accel.get().device_id;
-
 		if (status.hil_state == vehicle_status_s::HIL_STATE_ON) {
 			calibration_valid = true;
 
 		} else {
-			calibration_valid = (calibration::FindCurrentCalibrationIndex("ACC", device_id) >= 0);
+			calibration_valid = (calibration::FindCurrentCalibrationIndex("ACC", accel.get().device_id) >= 0);
 		}
 
 		if (!calibration_valid) {

src/modules/commander/Arming/PreFlightCheck/checks/gyroCheck.cpp
@@ -61,7 +61,7 @@ bool PreFlightCheck::isGyroRequired(const uint8_t instance)
 }
 
 bool PreFlightCheck::gyroCheck(orb_advert_t *mavlink_log_pub, vehicle_status_s &status, const uint8_t instance,
-			       const bool optional, int32_t &device_id, const bool report_fail)
+			       const bool optional, const bool report_fail)
 {
 	const bool exists = (orb_exists(ORB_ID(sensor_gyro), instance) == PX4_OK);
 	bool calibration_valid = false;
@@ -79,13 +79,11 @@ bool PreFlightCheck::gyroCheck(orb_advert_t *mavlink_log_pub, vehicle_status_s &
 			}
 		}
 
-		device_id = gyro.get().device_id;
-
 		if (status.hil_state == vehicle_status_s::HIL_STATE_ON) {
 			calibration_valid = true;
 
 		} else {
-			calibration_valid = (calibration::FindCurrentCalibrationIndex("GYRO", device_id) >= 0);
+			calibration_valid = (calibration::FindCurrentCalibrationIndex("GYRO", gyro.get().device_id) >= 0);
 		}
 
 		if (!calibration_valid) {

src/modules/commander/Arming/PreFlightCheck/checks/magnetometerCheck.cpp
@@ -61,7 +61,7 @@ bool PreFlightCheck::isMagRequired(const uint8_t instance)
 }
 
 bool PreFlightCheck::magnetometerCheck(orb_advert_t *mavlink_log_pub, vehicle_status_s &status, const uint8_t instance,
-				       const bool optional, int32_t &device_id, const bool report_fail)
+				       const bool optional, const bool report_fail)
 {
 	const bool exists = (orb_exists(ORB_ID(sensor_mag), instance) == PX4_OK);
 	bool calibration_valid = false;
@@ -80,13 +80,11 @@ bool PreFlightCheck::magnetometerCheck(orb_advert_t *mavlink_log_pub, vehicle_st
 			}
 		}
 
-		device_id = magnetometer.get().device_id;
-
 		if (status.hil_state == vehicle_status_s::HIL_STATE_ON) {
 			calibration_valid = true;
 
 		} else {
-			calibration_valid = (calibration::FindCurrentCalibrationIndex("MAG", device_id) >= 0);
+			calibration_valid = (calibration::FindCurrentCalibrationIndex("MAG", magnetometer.get().device_id) >= 0);
 		}
 
 		if (!calibration_valid) {
@@ -98,7 +96,7 @@ bool PreFlightCheck::magnetometerCheck(orb_advert_t *mavlink_log_pub, vehicle_st
 		for (uint8_t i = 0; i < ORB_MULTI_MAX_INSTANCES; i++) {
 			uORB::SubscriptionData<estimator_status_s> estimator_status_sub{ORB_ID(estimator_status), i};
 
-			if (estimator_status_sub.get().mag_device_id == static_cast<uint32_t>(device_id)) {
+			if (estimator_status_sub.get().mag_device_id == static_cast<uint32_t>(magnetometer.get().device_id)) {
 				if (estimator_status_sub.get().control_mode_flags & (1 << estimator_status_s::CS_MAG_FAULT)) {
 					is_mag_fault = true;
 					break;